e7430b30394c9e0528d4a7dce702bd73119c3207
alicia-lyu <wlyu27@wisc.edu>
2023-08-04 17:00:50 -0500

    reorganize login code

--- app/auth/authUtils.tsx
+++ app/auth/authUtils.tsx
@@ -1,4 +1,27 @@
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
+import { RoleType } from "../context";
+import { useRouter } from "next/navigation";
+
 export enum AuthType {
     LOGIN = "login",
     SIGNUP = "signup"
+}
+
+export function extractForm(event: React.FormEvent<HTMLFormElement>): { 
+    username: string,
+    password: string,
+    role: RoleType,
+    router: AppRouterInstance
+ } {
+    const username = (event.currentTarget.elements.namedItem('username') as HTMLInputElement).value;
+    const password = (event.currentTarget.elements.namedItem('password') as HTMLInputElement).value;
+    const roleInput = (event.currentTarget.elements.namedItem('role') as HTMLInputElement).value;
+    const role: RoleType = roleInput === "applicant" ? RoleType.APPLICANT : RoleType.LAB;
+    const router = useRouter();
+    return {
+        username,
+        password,
+        role,
+        router
+    }
 }
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,59 @@
-"use client"
-
-import { AuthType } from "../authUtils";
+import { AuthType, extractForm } from "../authUtils";
 import AuthForm from "../form";
-import handleLogin from "./loginFunction";
+import { RoleType } from "@/app/context";
+
 
 export default function Login() {
     return <AuthForm handleLogin={handleLogin} functionality={AuthType.LOGIN} />
+}
+
+
+async function fakeLogin(username: string, password: string, role: RoleType) {
+    console.log("Faking login by sending request to badger chat...")
+    const res = await fetch("https://cs571.org/s23/hw6/api/login", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "X-CS571-ID": "bid_30e5ed25e99b26f8f91c",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password,
+            "role": role
+        })
+    })
+    return res;
+}
+
+function handleLogin(event: React.FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    const {
+        username,
+        password,
+        role,
+        router
+    } = extractForm(event);
+
+    if (username === "" || password === "") {
+        alert("You must provide both a username and password!");
+    } else {
+        // To-do: change fakeSignup to realSignup
+        fakeLogin(username, password, role).then((res) => {
+            console.log("Log in response code: ", res.status);
+            console.log("username: ", username);
+            if (res.status === 404) {
+                alert("Incorrect username!");
+            } else if (res.status === 401) {
+                alert("Incorrect password!");
+            } else if (res.status === 200) {
+                router.push(`/import?role=${role}`)
+            }
+            return res.json();
+        }).then((data) => {
+            console.log(data);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
 }
