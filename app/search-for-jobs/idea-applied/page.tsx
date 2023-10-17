42b4b5fe80fcfa80975d67f4c732bdb54b47b657
jkintana <jkintana@protonmail.com>
2023-10-17 01:19:52 -0500

    Finished idea applied

--- app/search-for-jobs/idea-applied/page.tsx
+++ app/search-for-jobs/idea-applied/page.tsx
@@ -1,15 +1,18 @@
-import { Button } from '@/client-wrappers/bootstrap';
+import { Container, Button } from '@/client-wrappers/bootstrap';
+
+import "./styles.scss";
 
 export default function IdeaApplied() {
     return (
-        <div>
-            <p>
+        <Container className="medium-container" id="container">
+            <h2>
                 Congrats! Your Idea has been submitted and the Ricke Lab team will get back to you with their feedback!
+                <br/>
                 Keep in mind that the ownership of your idea is protected by our legal team, and in case of any issues with your idea and collaboration, you can reach out to us.
-            </p>
+            </h2>
             <Button>
                 Back to Jobs Listings
             </Button>
-        </div>
+        </Container>
     )
 }
