722a0896d1bd8790a9948e5869ca0dba98a045af
Kiet Pham <paulpham.kiet@gmail.com>
2024-02-15 23:55:00 -0600

    Add test environment

---
+++ app/test-environment/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { BugReportButton } from "@/components/bug-report-buttons/button";
+
+export default function JobsPage() {
+    return (
+        <BugReportButton />
+    );
+}
