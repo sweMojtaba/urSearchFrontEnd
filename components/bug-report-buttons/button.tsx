85095bed2fe628a15a42269d38c2946793407dc9
jkintana <jkintana@protonmail.com>
2024-03-20 00:30:37 -0500

    Hydrated bug report button

--- components/bug-report-buttons/button.tsx
+++ components/bug-report-buttons/button.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
 import { Col, Row, Button, Container } from "@/client-wrappers/bootstrap"
 import { Form } from 'react-bootstrap'
 import './styles.scss';
@@ -8,6 +8,41 @@ export function BugReportButton() {
 
     const bugReportStatus = () => {
         setBugReportIsVisible(true)
+    }
+
+    async function sendReport(title: string, description: string) {
+        const url = String(process.env.NEXT_PUBLIC_API_URL) + "api/reportbug";
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "currentPage": "TESTING", // TODO implement using https://nextjs.org/docs/app/api-reference/functions/use-pathname
+                "title": title,
+                "description": description,
+            }),
+        });
+        return res;
+    }
+
+    // Retrieves the contents of the Title and Description forms.
+    function extractForm(event: React.FormEvent<HTMLFormElement>): { title: string, description: string } {
+        const title = (event.currentTarget.elements.namedItem("title") as HTMLInputElement).value;
+        const description = (event.currentTarget.elements.namedItem("description") as HTMLInputElement).value;
+        return { title, description }
+    }
+
+    // Fires after user presses submit button.
+    function submitReport(event: React.FormEvent<HTMLFormElement>): void {
+        event.preventDefault();
+        const { title, description } = extractForm(event);
+        const res = sendReport(title, description).then((res) => {
+            if (res.status === 200) { // TODO add additional error messages; use something other than alert
+                alert("Bug report sent!");
+                setBugReportIsVisible(false);
+            }
+        })
     }
 
     const useEscape = () => {
@@ -31,13 +66,13 @@ export function BugReportButton() {
         return (
             <Col xs={24} className={`popup-${bugReportPopUpIsVisible ? "visible" : "hidden"}`}>
                 <div className="overlay"></div>
-                <Form className="bug-report-form" style={{"zIndex": 102}}>      
+                <Form className="bug-report-form" style={{"zIndex": 102}} onSubmit={submitReport}>      
                     <h2>Report A Problem!</h2>
                     <Form.Group>
-                        <Form.Control as="input" type="text" placeholder="Title" className="report-form-title"/>
+                        <Form.Control as="input" type="text" placeholder="Title" className="report-form-title" name="title"/>
                     </Form.Group>
                     <Form.Group>
-                        <Form.Control as="textarea" rows={5}  placeholder="Description" className="report-form-description"/>
+                        <Form.Control as="textarea" rows={5}  placeholder="Description" className="report-form-description" name="description"/>
                     </Form.Group>
                     <div style={{"display": "flex"}}>
                         <div style={{"width": "50%"}}></div>
