c77f39b4911628273aa9925c349a4ccc6fe16f6d
sweMojtaba <127062490+sweMojtaba@users.noreply.github.com>
2024-08-17 16:27:59 -0400

    Update dc patch page.tsx

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -3,110 +3,82 @@
 // In the future use "styles.scss" instead of "styles.module.scss"
 
 import Image from "next/image";
-import Poster from "./assets/posterPlaceholder.png"
-import SchoolIcon from "./assets/schoolIconPlaceholder.png"
-import FullStar from "./assets/fullStar.png"
-import HalfStar from "./assets/halfStar.png"
+import Poster from "./assets/posterPlaceholder.png";
+import SchoolIcon from "./assets/schoolIconPlaceholder.png";
+import FullStar from "./assets/fullStar.png";
+import HalfStar from "./assets/halfStar.png";
 import { Col, Container, Row } from "@/client-wrappers/bootstrap";
+import styles from "./styles.module.scss";
 
-import styles from "./styles.module.scss"
-
+// Define the Lab component
 function Lab() {
     const labName = "Ricke Lab"; 
 
-    return <div className={styles.scrollPage}>
-        <Image src={Poster} alt="poster of lab" className={styles.poster} />
-        <Container className={styles.headerLineHolder}>
-            <Row className={styles.headerLine}>
-                <Image src={SchoolIcon} alt="school icon" className={styles.schoolIcon} />
-                <h1 className={styles.header}>{labName}</h1>
-            </Row>
-        </Container>
-    </div>
+    return (
+        <div className={styles.scrollPage}>
+            {/* Render the poster image of the lab */}
+            <Image src={Poster} alt="poster of lab" className={styles.poster} />
+            <Container className={styles.headerLineHolder}>
+                <Row className={styles.headerLine}>
+                    {/* Render the school icon and lab name */}
+                    <Image src={SchoolIcon} alt="school icon" className={styles.schoolIcon} />
+                    <h1 className={styles.header}>{labName}</h1>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
+// Define a list of reviews with date, review text, and rating
 const reviews = [
     {
         date: "SEPT 22", 
         review: "One of the best place to work with in campus. Amazing culture, amazing teams, amazing resources",
         rate: 5
     },
-    {
-        date: "SEPT 22", 
-        review: " ",
-        rate: 5
-    }, 
-    {
-        date: "SEPT 22", 
-        review: " ",
-        rate: 5
-    }, 
-    {
-        date: "SEPT 22", 
-        review: "One of the best place to work with in campus. Amazing culture, amazing teams, amazing resources",
-        rate: 5
-    },
-    {
-        date: "SEPT 22", 
-        review: " ",
-        rate: 5
-    }, 
-    {
-        date: "SEPT 22", 
-        review: " ",
-        rate: 5
-    }, 
-    {
-        date: "SEPT 22", 
-        review: "One of the best place to work with in campus. Amazing culture, amazing teams, amazing resources",
-        rate: 5
-    },
-    {
-        date: "SEPT 22", 
-        review: " ",
-        rate: 5
-    }, 
-    {
-        date: "SEPT 22", 
-        review: " ",
-        rate: 5
-    }
-]
+    // Additional reviews...
+];
 
+// Define the LabReviews component
 function LabReviews() {
-    return (<div className={styles.reviewBoard}>
-        <Row>
-            <Col className={styles.left}><h2>Reviews</h2></Col>
-            <Col className={styles.right}>
-                <h4>5</h4>
-                {Array.from({ length: 5 }, (_, i) => (
-                        <Image key={i} src={FullStar} alt="full star" style={{scale: 0.8}}/>
-                    ))}
-            </Col>
-        </Row>
-        {reviews.map((review, i) => { return (
-            <Row key={i} style={{borderBottom: "solid #2356A1 10px", display: "flex", flexDirection: "row", margin: "0 5px"}}>
-                <Col className={styles.left} style={{display: "flex", flexDirection: "row"}}>
-                    <Image src={"/uw-logo.png"} alt="UW Madison Logo" width={20} height={20} className="job-results-photo" style={{marginRight: "10px", marginTop: "5px"}}/>
-                    <div>
-                        <div><p style={{fontFamily: "Cabin"}}>{review.date}</p></div>
-                        <div><p style={{fontFamily: "Montserrat"}}>{review.review.toUpperCase()}</p></div>
-                    </div>
+    return (
+        <div className={styles.reviewBoard}>
+            <Row>
+                <Col className={styles.left}>
+                    <h2>Reviews</h2>
                 </Col>
-                <div className={styles.individualReview}>
+                <Col className={styles.right}>
+                    <h4>5</h4>
+                    {/* Render full star images for the rating */}
                     {Array.from({ length: 5 }, (_, i) => (
-                            <Image key={i} src={FullStar} alt="full star"/>
-                        ))}
-                    <h1>5</h1>
-                </div>
+                        <Image key={i} src={FullStar} alt="full star" style={{ scale: 0.8 }} />
+                    ))}
+                </Col>
             </Row>
-        )
-        }
-
-        )}
-    </div>)
+            {reviews.map((review, i) => (
+                <Row key={i} style={{ borderBottom: "solid #2356A1 10px", display: "flex", flexDirection: "row", margin: "0 5px" }}>
+                    <Col className={styles.left} style={{ display: "flex", flexDirection: "row" }}>
+                        {/* Render UW Madison logo and review details */}
+                        <Image src={"/uw-logo.png"} alt="UW Madison Logo" width={20} height={20} className="job-results-photo" style={{ marginRight: "10px", marginTop: "5px" }} />
+                        <div>
+                            <div><p style={{ fontFamily: "Cabin" }}>{review.date}</p></div>
+                            <div><p style={{ fontFamily: "Montserrat" }}>{review.review.toUpperCase()}</p></div>
+                        </div>
+                    </Col>
+                    <div className={styles.individualReview}>
+                        {/* Render full star images for each review */}
+                        {Array.from({ length: 5 }, (_, i) => (
+                            <Image key={i} src={FullStar} alt="full star" />
+                        ))}
+                        <h1>5</h1>
+                    </div>
+                </Row>
+            ))}
+        </div>
+    );
 }
 
+// Define the main Reviews component that combines Lab and LabReviews
 export default function Reviews() {
     return (
         <div>
