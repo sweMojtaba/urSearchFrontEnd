f8d78399817ae67f4733e30645859eb643c4cbcc
Matthew Gehrt <matthewagehrt@gmail.com>
2024-11-03 20:06:48 -0600

    40200 - applicant profile viewing done

--- app/applicant-profile/page.tsx
+++ app/applicant-profile/page.tsx
@@ -1,4 +1,4 @@
-// "use client" // needed for ApplicantExclusion wrapper
+"use client" // needed for ApplicantExclusion wrapper
 // The "use client" directive is commented out but typically needed when using client-side rendering in Next.js components
 
 // Importing Col, Container, and Row components from a custom bootstrap wrapper
@@ -6,12 +6,65 @@ import { Col, Container, Row } from "@/client-wrappers/bootstrap";
 
 // Importing various sections/components of the profile page from the profileSections module
 import { Accomplishments, Affiliations, Documents, Experiences, PersonalInfo, Projects, QuickApply, Skills, Video } from "./profileSections";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 // Importing a higher-order component for user exclusion logic, which is currently commented out
 // import { withApplicantExclusion } from "@/utils/withUserExclusion";
 
+const defaultJobseeker = {
+    name: "",
+    degree: "",
+    major: "",
+    school: "",
+    classYear: 0,
+    GPA: 0,
+    phone: 123456789,
+    email: "",
+    isQuickApplyActivated: false,
+    GPAhidden: false,
+    files: [],
+    videos: [],
+    affiliations: [],
+    experiences: [],
+    projects: [],
+    skills: [],
+    accomplishments: []
+}
+
 // Define the Profile function component
 function Profile() {
+    // Retrieve search parameters from URL
+    const searchParams = useSearchParams();
+    const jobseekerId = searchParams.get("ID") || ""; // Extract the jobseeker ID from the URL
+
+    const [jobseekerData, setJobseekerData] = useState<any>(defaultJobseeker);
+
+    useEffect(() => {
+        const baseUrl = "https://ursearch-api.salmonmeadow-33eeb5e6.westus2.azurecontainerapps.io/";
+        // const baseUrl = process.env.NEXT_PUBLIC_API_URL; // Optionally use environment variable
+        const url = baseUrl + `api/jobseeker/${jobseekerId}`;
+        const getJobseekerData = async () => {
+            try {
+                const res = await fetch(url, {
+                    method: "GET",
+                    credentials: "include",
+                });
+                const data = await res.json();
+                if (res.status === 200 && data.code === 0) {
+                    setJobseekerData(data.data.person); // Set jobseeker data state
+                } else {
+                    console.log(data.msg); // Log error message if not successful
+                }
+            } catch (error) {
+                console.error("Error fetching jobseeker data:", error); // Catch and log fetch errors
+            }
+        };
+
+        getJobseekerData();
+    }, [jobseekerId])
+
+
     // Return the layout of the profile page using a Container component
     return (
         <Container className="scroll-page">
@@ -20,29 +73,29 @@ function Profile() {
                 {/* First column for personal information, documents, and affiliations */}
                 <Col md={4} xl={3}>
                     {/* Render the PersonalInfo component */}
-                    <PersonalInfo />
+                    <PersonalInfo jobseekerData={jobseekerData} />
                     {/* Render the Documents component */}
-                    <Documents />
+                    <Documents documents={jobseekerData.files} />
                     {/* Render the Affiliations component */}
-                    <Affiliations />
+                    <Affiliations affiliations={jobseekerData.affiliations} />
                 </Col>
                 {/* Second column for experiences, projects, and video */}
                 <Col md={8} xl={6}>
                     {/* Render the Experiences component */}
-                    <Experiences />
+                    <Experiences experiences={jobseekerData.experiences} />
                     {/* Render the Projects component */}
-                    <Projects />
+                    <Projects projects={jobseekerData.projects} />
                     {/* Render the Video component */}
-                    <Video />
+                    <Video video={jobseekerData.videos[0]} />
                 </Col>
                 {/* Third column for skills, accomplishments, and quick apply */}
                 <Col md={12} xl={3} id="lastCol">
                     {/* Render the Skills component */}
-                    <Skills />
+                    <Skills skills={jobseekerData.skills} />
                     {/* Render the Accomplishments component */}
-                    <Accomplishments />
+                    <Accomplishments accomplishments={jobseekerData.accomplishments} />
                     {/* Render the QuickApply component with quickApply prop set to true */}
-                    <QuickApply quickApply={true}/>
+                    <QuickApply quickApply={jobseekerData.isQuickApplyActivated}/>
                     {/* TO-DO: fetch data in server component */}
                 </Col>
             </Row>
--- app/applicant-profile/profileSections.tsx
+++ app/applicant-profile/profileSections.tsx
@@ -2,42 +2,23 @@
 
 // Importing necessary components and functions
 import { CardWithImg, InfoCard, PassableInfoCardProps } from "@/components/cards-and-items/cards";
-import { fetchAccomplishments, fetchAffiliations, fetchDocuments, fetchExperiences, fetchGPAhidden, fetchPersonalInfo, fetchProjects, fetchSkills, fetchVideo } from "./fetchProfileSections";
 import { BigLi, SmallLi } from "@/components/cards-and-items/listItems";
 import Profile from "./profileSolid.svg";
-import { useContext, useMemo, useState } from "react";
-import { UserContext } from "../context";
+import { useMemo } from "react";
 import WorkIcon from "./work.svg";
 import ProjectIcon from "./project.svg";
 import fakeResponse from "@/utils/fakeResponse";
 import { ButtonTextEnum, QuickApplyTemplate, createButtonStatus } from "@/components/functionalities/quickApply";
-import { Button } from 'react-bootstrap';
 
 // Component to display personal information
-export function PersonalInfo() {
-    // Accessing user context
-    const { user, setUser } = useContext(UserContext);
-    // Fetching personal information using useMemo for memoization
-    const {
-        name,
-        degree,
-        major,
-        school,
-        classYear,
-        GPA,
-        phone,
-        email
-    } = useMemo(fetchPersonalInfo, [user]);
-    // Fetching GPA visibility status
-    const { GPAhidden } = useMemo(fetchGPAhidden, [user]);
-    
+export function PersonalInfo(jobseekerData: any) {
     // Defining properties for the card component
     const cardProps: PassableInfoCardProps = useMemo(() => {
         return {
-            title: name,
+            title: jobseekerData.name,
             editFunc: () => { console.log("TO-DO: edit personal info") }
         }
-    }, [name]);
+    }, [jobseekerData.name]);
 
     // Returning the card with personal information
     return <CardWithImg
@@ -45,56 +26,45 @@ export function PersonalInfo() {
         cardProps={cardProps}
         ImgSrc={Profile}
     >
-        <p>{degree} {major}</p>
-        <p>{school}</p>
-        <p>class of {classYear}</p>
-        <p className="stand-out">GPA: {GPA} <span className="secondary-text">{GPAhidden ? "hidden" : "shown"}</span></p>
-        <p>phone: {phone}</p>
-        <p>email: {email}</p>
+        <p>{jobseekerData.degree} {jobseekerData.major}</p>
+        <p>{jobseekerData.school}</p>
+        <p>class of {jobseekerData.classYear}</p>
+        <p className="stand-out">GPA: {jobseekerData.GPA} <span className="secondary-text">{jobseekerData.GPAhidden ? "hidden" : "shown"}</span></p>
+        <p>phone: {jobseekerData.phone}</p>
+        <p>email: {jobseekerData.email}</p>
     </CardWithImg>;
 }
 
 // Component to display documents
-export function Documents() {
-    // Fetching documents using useMemo for memoization
-    const documents = useMemo(fetchDocuments, []);
-
+export function Documents({ documents }: { documents: any[] }) {
     // Returning the card with documents information
     return <InfoCard
         title="Documents"
         editFunc={() => { console.log("TO-DO: edit documents") }}
     >
-        {documents.map(document => <SmallLi text={document.name} url={document.url} key={document.name} />)}
+        {documents.map((document: any) => <SmallLi text={document.name} url={document.url} key={document.name} />)}
     </InfoCard>;
 }
 
 // Component to display affiliations
-export function Affiliations() {
-    // State to trigger reload
-    const [reload, setReload] = useState(false);
-    // Fetching affiliations using useMemo for memoization
-    const affiliations = useMemo(fetchAffiliations, [reload]);
-
+export function Affiliations({ affiliations }: { affiliations: any[] }) {
     // Returning the card with affiliations information
     return <InfoCard
         title="Affiliations"
         editFunc={() => { console.log("TO-Do: edit affiliations") }}
     >
-        {affiliations.map(affiliation => <SmallLi text={affiliation.name} url={affiliation.url} key={affiliation.name} />)}
+        {affiliations.map((affiliation: any) => <SmallLi text={affiliation.name} url={affiliation.url} key={affiliation.name} />)}
     </InfoCard>;
 }
 
 // Component to display experiences
-export function Experiences() {
-    // Fetching experiences using useMemo for memoization
-    const experiences = useMemo(fetchExperiences, []);
-
+export function Experiences({ experiences }: { experiences: any[] }) {
     // Returning the card with experiences information
     return <InfoCard
         title="Experiences"
         editFunc={() => { console.log("TO-DO: edit experiences") }}
     >
-        {experiences.map((experience) => {
+        {experiences.map((experience: any) => {
             return <BigLi
                 key={experience.name}
                 title={experience.name}
@@ -107,16 +77,13 @@ export function Experiences() {
 }
 
 // Component to display projects
-export function Projects() {
-    // Fetching projects using useMemo for memoization
-    const projects = useMemo(fetchProjects, []);
-
+export function Projects({ projects }: { projects: any[] }) {
     // Returning the card with projects information
     return <InfoCard
         title="Projects and Publications"
         editFunc={() => { console.log("TO-DO: edit experiences") }}
     >
-        {projects.map((project) => {
+        {projects.map((project: any) => {
             return <BigLi
                 key={project.name}
                 title={project.name}
@@ -129,10 +96,7 @@ export function Projects() {
 }
 
 // Component to display video
-export function Video(): JSX.Element {
-    // Fetching video URL using useMemo for memoization
-    const video = useMemo(fetchVideo, []);
-
+export function Video(video: any): JSX.Element {
     // Returning the card with video player
     return <InfoCard
         title="Let me introduce myself..."
@@ -145,34 +109,24 @@ export function Video(): JSX.Element {
 }
 
 // Component to display skills
-export function Skills() {
-    // State to trigger reload
-    const [reload, setReload] = useState(false);
-    // Fetching skills using useMemo for memoization
-    const skills = useMemo(fetchSkills, [reload]);
-
+export function Skills({ skills }: { skills: any[] }) {
     // Returning the card with skills information
     return <InfoCard
         title="Skills"
         editFunc={() => { console.log("TO-Do: edit skills") }}
     >
-        {skills.map(skill => <SmallLi text={skill} url="" key={skill} />)}
+        {skills.map((skill: any) => <SmallLi text={skill} url="" key={skill} />)}
     </InfoCard>;
 }
 
 // Component to display accomplishments
-export function Accomplishments() {
-    // State to trigger reload
-    const [reload, setReload] = useState(false);
-    // Fetching accomplishments using useMemo for memoization
-    const accomplishments = useMemo(fetchAccomplishments, [reload]);
-
+export function Accomplishments({ accomplishments }: { accomplishments: any[] }) {
     // Returning the card with accomplishments information
     return <InfoCard
         title="Accomplishments"
         editFunc={() => { console.log("TO-Do: edit skills") }}
     >
-        {accomplishments.map(accomplishment => <SmallLi text={accomplishment} url="" key={accomplishment} />)}
+        {accomplishments.map((accomplishment: any) => <SmallLi text={accomplishment} url="" key={accomplishment} />)}
     </InfoCard>;
 }
 
